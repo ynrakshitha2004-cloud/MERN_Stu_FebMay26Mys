29596e5f9bdf98ade8dad27f3c33d7ac0e5224ec
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-03 11:28:36 +0530

    Console usage & simple DOM manipulation

---
+++ W2/w3/W3/D2/p2.js
@@ -0,0 +1,17 @@
+//comparison operators
+
+console.log("comparison operators");
+let  x =5, y = '5';
+console.log("x==y:",x==y); //Loose equality
+console.log("x===y:",x===y); //strict equality
+console.log("x!=y:",x!=y); //Loose inequality
+console.log("x!==y:",x!==y); //Loose equality
+console.log("x>3:",x>3); 
+console.log("x<:",x<3); 
+console.log("x>=y:",x>=3); 
+console.log("x<=y:",x<=3); 
+
+
+
+
+
---
+++ W2/w3/W3/D2/p4.js
@@ -0,0 +1,11 @@
+//condition statements
+let age = 20;
+if(age < 13){
+    console.log("child");
+}
+else if(age<18){
+    console.log("Teenager");
+}
+else{
+    console.log("Adult");
+}
