0d1adaa772f20fa436792f33239a306140901065
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-03 15:44:26 +0530

    comparison operators

---
+++ W3/D2/p2.js
@@ -0,0 +1,17 @@
+//comparison operators
+
+console.log("comparison operators");
+let  x =10, y = 3;
+console.log("x==y:",x==y); //Loose equality
+console.log("x===y:",x===y); //strict equality to
+console.log("x!=y:",x!=y); //Loose inequality
+console.log("x!==y:",x!==y); //strict Not equal
+console.log("x>3:",x>3); //Greater than
+console.log("x<:",x<3); //Lesser than
+console.log("x>=y:",x>=3); //Greater than or equal to
+console.log("x<=y:",x<=3);//Less than or equal to 
+
+
+
+
+
