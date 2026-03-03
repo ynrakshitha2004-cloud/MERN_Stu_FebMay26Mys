092f6b9160b58311725dd8c9f26b2ed272817bc4
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-03 22:35:21 +0530

    Recursive function

---
+++ W3/D2/p11.js
@@ -0,0 +1,10 @@
+// Recursive function
+// A function which calls itself
+
+function factorial(n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+console.log("Factorial of 5: ", factorial(5));
