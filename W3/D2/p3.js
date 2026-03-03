c912531cc1817322349e9719edd2ccaddf81a671
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-03 15:46:20 +0530

    Logical operators

---
+++ W3/D2/p3.js
@@ -0,0 +1,6 @@
+//Logical operators
+console.log("Logical operators");
+let a = true, b = false
+console.log("a&&b=" ,a&&b);//Logical AND
+console.log("a||",a||b); //Loose OR
+console.log("!a",!a); //Loose NOT
