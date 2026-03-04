214199aa069c190078654c40297442377edf6da6
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-04 12:45:50 +0530

    Basics of Object

---
+++ W3/D3/p5.js
@@ -0,0 +1,12 @@
+// Basics of Objects
+const Person = {
+    name:"Rakshi",
+    age:22,
+    isStudent: false
+};
+
+console.log(" Name :",Person.name);
+console.log(" Name :",Person["age"]);
+
+
+
