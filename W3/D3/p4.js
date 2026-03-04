a81f3f7f10e38867c574ffd0d58a6fd75baa035b
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-04 12:43:02 +0530

    step size

---
+++ W3/D3/p4.js
@@ -0,0 +1,8 @@
+// 1.step size
+for(let i =0;i<=10;i+=2){
+    //console.log("i =",i);
+}
+//2. Two counters in one for loop
+for(let i=1, j=5; i<=5 && j>=1; i++,j--){
+    console.log("i:", i ," j: ", j);
+}
