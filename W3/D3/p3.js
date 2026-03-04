34b9f723bc4c6de647e64dbb119ca97de41c3c51
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-04 12:42:10 +0530

    forward loop continue

---
+++ W3/D3/p3.js
@@ -0,0 +1,16 @@
+for(let i=0;i<5;i++){//forward loop
+    if(i===2){
+    console.log("i",i);
+    break;
+}
+}
+
+//continue
+
+for(let i=0;i<5;i++){//
+    if(i===2){
+    //console.log("i",i);
+    continue;
+}
+console.log("i",i);
+}
