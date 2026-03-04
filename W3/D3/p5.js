4cbabe3b64c7136a773b5190add94aa3d0a4a3dd
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-04 14:27:39 +0530

    Basics of Object

--- W3/D3/p5.js
+++ W3/D3/p5.js
@@ -8,5 +8,19 @@ const Person = {
 console.log(" Name :",Person.name);
 console.log(" Name :",Person["age"]);
 
+//Add a new property
+Person.city = "Mysore";
+console.log(" Person :",Person);
+//Modify
+Person.age = 31;
+//delete person
+delete Person.isStudent;
+console.log(" Person ",Person);
 
+//Object constructor
+const car = new Object();
+car.Make = "Audi";
+car.Model = "A4";
+car.year = 2026;
+console.log("car:",car);
 
