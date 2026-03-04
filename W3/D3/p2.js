25d5c0be410aff02ed450d318f9113919ee6f7a9
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-04 12:40:37 +0530

    while loop

---
+++ W3/D3/p2.js
@@ -0,0 +1,22 @@
+//while loop 
+//while(condition){
+    //code to execute 
+   // increment/decrement}
+
+   console.log("while loop");
+   let count = 0;
+   while(count<5){
+    console.log("count: ",count);
+    count++;
+   }
+
+   //do{
+   //code to execute 
+   //inc/dec
+   //}while(condition);
+   //do while
+   let value = 10;
+   do{
+   console.log("Value :",value);
+   value++;
+   }while(value < 3);
