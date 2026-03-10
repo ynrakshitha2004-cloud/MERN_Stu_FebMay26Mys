436f4a46bfa80b8a6bf51d24712da00167556e67
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-10 21:42:36 +0530

    Password required/Email validation

--- W4/D2/p5.js
+++ W4/D2/p5.js
@@ -1,18 +1,94 @@
-const signupForm = document.getElementById("signupForm");
-const signupemail = document.getElementById("signupemail");
-const signuppassword = document.getElementById("signuppassword");
-const message = document.getElementById("message");
+signupForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
+    const email = signupemail.value;
+    const password = signuppassword.value;
 
-signupForm.addEventListener("submit",
-    function(event){
-        event.preventDefault();
-
-        if(!email){
-            message.textContent = "Email is Required";
-            message.style.color = "blue";
-            signupemail.focus();
-            return;
-        }
+    // Email validation
+    if (!email) {
+        message.textContent = "Email is required";
+        message.style.color = "blue";
+        signupemail.focus();
+        return;
     }
-)
+
+    if (!email.includes("@") || !email.includes(".")) {
+        message.textContent = "Please enter valid email id";
+        message.style.color = "red";
+        signupemail.focus();
+        return;
+    }
+
+    // Password required
+    if (!password) {
+        message.textContent = "Password is required";
+        message.style.color = "red";
+        signuppassword.focus();
+        return;
+    }
+
+    // Password length
+    if (password.length < 8) {
+        message.textContent = "Password must be at least 8 characters";
+        message.style.color = "red";
+        signuppassword.focus();
+        return;
+    }
+
+    // Uppercase check
+    if (!/[A-Z]/.test(password)) {
+        message.textContent = "Password must have at least 1 UPPERCASE character";
+        message.style.color = "red";
+        signuppassword.focus();
+        return;
+    }
+
+    // Lowercase check
+    if (!/[a-z]/.test(password)) {
+        message.textContent = "Password must have at least 1 lowercase character";
+        message.style.color = "red";
+        signuppassword.focus();
+        return;
+    }
+
+    // Number check
+    if (!/\d/.test(password)) {
+        message.textContent = "Password must have at least 1 digit";
+        message.style.color = "red";
+        signuppassword.focus();
+        return;
+    }
+
+    // Special character check
+    if (!/[@#$%&*!]/.test(password)) {
+        message.textContent = "Password must have at least 1 special character";
+        message.style.color = "red";
+        signuppassword.focus();
+        return;
+    }
+    const confirmPassword = confirmpassword.value;
+
+// confirm pass
+if (!confirmPassword) {
+    message.textContent = "Please confirm your password";
+    message.style.color = "red";
+    confirmpassword.focus();
+    return;
+}
+
+
+if (password !== confirmPassword) {
+    message.textContent = "Passwords do not match";
+    message.style.color = "red";
+    confirmpassword.focus();
+    return;
+}
+
+    message.textContent = "valid email & password entered ";
+    message.style.color = "green";
+    console.log("success!",{email,password:"***Hidden***"});
+
+});
+//clear message on input
+signupemail.addEventListener("input" ,() => message.textcontent = "");
+signuppassword.addEventListener("input" ,() => message.textcontent = "");
