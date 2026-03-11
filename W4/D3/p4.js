b80bc3f70b79f4515c50980dca7a6724155b6808
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-11 14:47:31 +0530

    storing and retreiving json data

---
+++ W4/D3/p4.js
@@ -0,0 +1,23 @@
+const saveBtn = document.getElementById("saveBtn");
+const jsonOutput = document.getElementById("jsonOutput");
+
+saveBtn.addEventListener("click", function () {
+
+    const user = {
+        name: "Rakshitha",
+        role: "CSE Student",
+        id: 101,
+        skills: ["Js", "HTML", "CSS"]
+    };
+
+    localStorage.setItem("userprofile", JSON.stringify(user));
+    jsonOutput.textContent = "User object stored as string to localStorage";
+});
+
+document.getElementById("readBtn").addEventListener("click",function(){
+    const username = localStorage.getItem("userProfile");
+    console.log(JSON.parse(username))
+    console.log(username);
+    jsonOutput.textContent = " userProfile" + username;
+    
+});
