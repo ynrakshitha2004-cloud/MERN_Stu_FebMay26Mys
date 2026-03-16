28980c7ad66a9d43c303e586a6ca5f9b621b2590
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-16 23:15:34 +0530

    Array destructing

---
+++ W5/D1/P1.js
@@ -0,0 +1,17 @@
+// Introduction to jnode.js
+
+const runtimeName = "Node.js";
+//console.log("Introduction to Node.js");
+console.log(`${runtimeName} runs javascript outside the browser`);
+
+const commonUses = [
+    "used for server-side app",
+    "automation scripts can be created"
+
+];
+//Array destructuring
+//console.log(commonUses[0]);
+//console.log(commonUses[1]);
+commonUses.forEach((commonUse,index) =>{
+    console.log(`${index+1}.${commonUse}`);
+});
