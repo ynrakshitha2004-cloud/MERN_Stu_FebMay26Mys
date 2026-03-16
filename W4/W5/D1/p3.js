78104aa2c3f9e579b0d4a7c110e20879977f0119
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-16 16:32:35 +0530

    setTimeOut

---
+++ W4/W5/D1/p3.js
@@ -0,0 +1,8 @@
+console.log("Node js architecture demo");
+console.log("1. Script started");
+
+//setTimeout
+setTimeout(()=>{
+    console.log("3. Timer callback finished after waiting");
+},1000);
+console.log("2. Script continued without waiting for timer callback");
