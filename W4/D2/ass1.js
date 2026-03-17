63d77fe7ac53b1d04cecd7e7dc83b49cb1d73703
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-17 08:59:39 +0530

    filterProject

--- W4/D2/ass1.js
+++ W4/D2/ass1.js
@@ -6,80 +6,78 @@ const error = document.getElementById("error");
 
 addBtn.addEventListener("click", function () {
 
-let question = questionInput.value.trim();
-let answer = answerInput.value.trim();
+    let question = questionInput.value.trim();
+    let answer = answerInput.value.trim();
 
-error.textContent = "";
-
-
-
-if (question.length < 5) {
-error.textContent = "Question must be at least 5 characters";
-return;
-}
-
-if (answer.length < 15) {
-error.textContent = "Answer must be at least 15 characters";
-return;
-}
+    error.textContent = "";
 
 
 
-const emptyMsg = document.getElementById("emptyMsg");
-if (emptyMsg) {
-emptyMsg.remove();
-}
+    if (question.length < 5) {
+        error.textContent = "Question must be at least 5 characters";
+        return;
+    }
 
-const faq = document.createElement("div");
-faq.className = "faq";
-
-const q = document.createElement("h4");
-q.textContent = question;
-
-
-q.insertAdjacentHTML(
-"beforeend",
-`<span class="badge">FAQ</span>`
-);
+    if (answer.length < 15) {
+        error.textContent = "Answer must be at least 15 characters";
+        return;
+    }
 
 
 
-const a = document.createElement("p");
-a.textContent = answer;
+    const emptyMsg = document.getElementById("emptyMsg");
+    if (emptyMsg) {
+        emptyMsg.remove();
+    }
+
+    const faq = document.createElement("div");
+    faq.className = "faq";
+
+    const q = document.createElement("h4");
+    q.textContent = question;
+
+
+    q.insertAdjacentHTML(
+        "beforeend",
+        `<span class="badge">FAQ</span>`
+    );
+
+
+    const a = document.createElement("p");
+    a.textContent = answer;
+
+
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "Delete";
 
 
 
-const delBtn = document.createElement("button");
-delBtn.textContent = "Delete";
+    delBtn.addEventListener("click", function () {
+        faq.remove();
+
+        if (faqContainer.children.length === 0) {
+
+            const msg = document.createElement("p");
+            msg.id = "emptyMsg";
+            msg.textContent = "No FAQs available";
+
+            faqContainer.appendChild(msg);
+
+        }
+
+    });
 
 
 
-delBtn.addEventListener("click", function () {
-faq.remove();
+    faq.appendChild(q);
+    faq.appendChild(a);
+    faq.appendChild(delBtn);
 
-if (faqContainer.children.length === 0) {
-
-const msg = document.createElement("p");
-msg.id = "emptyMsg";
-msg.textContent = "No FAQs available";
-
-faqContainer.appendChild(msg);
-
-}
-
-});
+    faqContainer.appendChild(faq);
 
 
 
-faq.appendChild(q);
-faq.appendChild(a);
-faq.appendChild(delBtn);
-
-faqContainer.appendChild(faq);
-
-
-
-questionInput.value = "";
-answerInput.value = "";
+    questionInput.value = "";
+    answerInput.value = "";
 
 });
