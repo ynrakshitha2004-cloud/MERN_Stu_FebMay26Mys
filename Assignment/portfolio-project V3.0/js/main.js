2ce0d2c265ed9f7f0b040b6d6a9f63b8dbbcd3e9
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-30 21:23:28 +0530

    Portfolio  V3.0

---
+++ Assignment/portfolio-project V3.0/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",function(){
+    //Render renderSkills
+    renderSkills(skills);
+    renderFilters(skills);
+    initModal();
+    initContactValidation();
+    initThemeToggle();
+    filterProject();
+    searchProject();
+    typeEffect();
+    
+})
