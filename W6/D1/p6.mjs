58cb0e519e8987147527c5db23275a2c50ccfd67
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-30 11:42:07 +0530

    Import ES modules

--- W6/D1/p6.mjs
+++ W6/D1/p6.mjs
@@ -1,9 +1,8 @@
-// Import ES modules
+// Import ES moduless
 import createInvoiceLabel, { calculateTotal, taxRate } from "./p5.mjs";
 const subtotal = 400;
 const total = calculateTotal(subtotal);
 const label = createInvoiceLabel("INV-2026-001");
-
 console.log("Invoice Label:", label);
 console.log("Tax rate:", taxRate);
 console.log("Final total:", total);
