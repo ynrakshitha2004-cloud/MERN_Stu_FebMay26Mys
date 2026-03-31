b44fc8351e06da118507db3872815a8c63e17b77
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-03-31 16:11:20 +0530

    deleting this files

--- portfolio-project/js/features/scroll-progress.js
+++
@@ -1,8 +0,0 @@
-const bar = document.getElementById("progressBar");
-
-window.addEventListener("scroll", () => {
-  const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
-  const progress = (window.scrollY / totalHeight) * 100;
-
-  bar.style.width = progress + "%";
-});
