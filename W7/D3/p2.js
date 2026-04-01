8336492ddd97a0fb80374fa5b49c70d29b73dd9f
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-01 11:10:29 +0530

    /Handling asynchronous errors with callbacks

---
+++ W7/D3/p2.js
@@ -0,0 +1,28 @@
+//Handling asynchronous errors with callbacks
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+app.get("/file", function (req, res, next) {
+    const filePath = path.join(__dirname, "newFile.txt");
+    fs.readFile(filePath, "utf-8", function (error, data) {
+        if (error) {
+            //Forwarding the async error
+            return next(error);
+        }
+        res.send(data);
+
+    });
+});
+//Centralize error handling middleware
+app.use(function (error, req, res, next) {
+    res.status(404).json({
+        success: false,
+        message: "File/folder does not exist.",
+        //OriginalMessage:error.message
+    });
+});
+app.listen(4000, function () {
+    console.log("Express server running at http://localhost:4000");
+});
