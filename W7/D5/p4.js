bc9f47148bd7978f2977b2a5a34aa0a040663847
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-03 16:46:40 +0530

    Basics RBAC

--- W7/D5/p4.js
+++ W7/D5/p4.js
@@ -9,8 +9,6 @@ function canAccess(route, role) {
     const allowedRoles = routePermissions[route] || []; // fallback if route not found
     return allowedRoles.includes(role);
 }
-
-// Test cases
 console.log("User access to /admin: ", canAccess("/admin", "user"));       
 console.log("Admin access to /admin: ", canAccess("/admin", "admin"));     
 console.log("Manager access to /reports: ", canAccess("/reports", "manager")); 
