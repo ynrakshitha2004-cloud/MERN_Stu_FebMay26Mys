bb978214687b9600e1393ee5887676dfd61e8e43
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-09 18:40:04 +0530

    Handles request related to movie

--- projects/Guided project/BookMyShowClone/MERN_Week2_V1/routes/movieRoutes.js
+++ projects/Guided project/BookMyShowClone/MERN_Week2_V1/routes/movieRoutes.js
@@ -1,28 +1,28 @@
-//Handles request related to movie
+// Handles requests related to movie
 const express = require("express");
 const { authMiddleware } = require("../middleware/authMiddleware");
-const { 
+const {
     getHome,
+    getAllMovies,
     getMovieById,
     addMovie,
     updateMovie,
-    deletedMovie
-
+    deleteMovie
 } = require("../controllers/movieController");
 const roleMiddleware = require("../middleware/roleMiddleware");
+
 const router = express.Router();
-//sends request to home page
+//Sends req to home page
 router.get("/",getHome);
-//send request to get all movies
+//Sends req to get all movies
 router.get("/movies",getAllMovies);
-//sends req to get movies based on id
+//Sends req to get movies based on id
 router.get("/movies/:id",getMovieById);
-//sends req to create new movie 
+//Sends req to create new movie
 router.post("/movies",authMiddleware,roleMiddleware("admin"),addMovie);
-//sends req to create add movie 
+//Sends req to update movie detail/s
 router.put("/movies/:id",authMiddleware,roleMiddleware("admin"),updateMovie);
-//sends req to update movie 
-router.delete("/movies",authMiddleware,roleMiddleware("admin"),deletedMovie);
+//Sends req to delete a movie
+router.delete("/movies/:id",authMiddleware,roleMiddleware("admin"),deleteMovie);
+
 module.exports = router;
-
-
