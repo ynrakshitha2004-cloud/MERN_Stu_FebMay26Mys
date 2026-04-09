06018d3653d10c6ef000693e7ddce292aea37bc7
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-09 11:44:07 +0530

    User's details of the application

---
+++ projects/Guided project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,19 @@
+//User's details of the application
+const users = [
+    {
+        id:1,
+        name:"Mahesh",
+        email:"u@e.com",
+        password:"mahi123",
+        role:"user"
+    },
+    {
+        id:2,
+        name:"Admin",
+        email:"a@e.com",
+        password:"admin123",
+        role:"admin"
+    }
+];
+
+module.exports = users;
