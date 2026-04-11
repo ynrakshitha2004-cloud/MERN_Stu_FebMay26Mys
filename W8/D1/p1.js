19b977b4e0f431a090a449fe46dd452082abc779
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-11 10:14:57 +0530

    Applied filters to the query using comparison operators

---
+++ W8/D1/p1.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+async function main(){
+    try{
+       await mongoose.connect("mongodb://localhost:27017/abcmern");
+       console.log("MongoDB connected successfully");
+       const userSchema = new mongoose.Schema({
+        name:String,
+        age:Number,
+        role:String
+       });
+       const User = mongoose.model("User",userSchema);
+       console.log("mongoose schema & model created successfully");
+       console.log("Model name:",user.modelName);
+       await mongoose.connection.close();
+       console.log("connection closed");
+       
+    }
+    catch(error){
+      console.log("Failed to connect to DB",error.message);
+    }
+}
+main();
