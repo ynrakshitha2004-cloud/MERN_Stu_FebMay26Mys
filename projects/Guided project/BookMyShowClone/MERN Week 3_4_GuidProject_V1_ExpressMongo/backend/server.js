f0de7f6b5b1a5ddde3944abdfaf1b12492fb9ed1
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-21 10:23:32 +0530

    DB Connection function invoked

--- projects/Guided project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
+++ projects/Guided project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
@@ -1,6 +1,9 @@
 //Load environment variables
 require("dotenv").config();
 const app = require("./app");
+const connectDB = require("./src/config/db");
+//connectDB();
+//port config
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Server ruuning on port ${PORT}`);
