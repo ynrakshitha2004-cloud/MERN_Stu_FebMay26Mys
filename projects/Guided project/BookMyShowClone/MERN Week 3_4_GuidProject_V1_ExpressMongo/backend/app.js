a2c278601e9d134917d8399088e8b133d676f091
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-22 10:20:40 +0530

    imported movie and booking routes and added to app.use

--- projects/Guided project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ projects/Guided project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const authRoutes = require("./src/routes/auth.routes");
-
+const movieRoutes = require("./src/routes/movie.routes")
+const bookingRoutes = require("./src/routes/booking.routes")
 const app = express();
 
 app.use(express.json());
 app.use("/api/auth",authRoutes);
-
+app.use("/api/movies",movieRoutes);
+app.use("/api/bookings",bookingRoutes);
 //Base URL
 app.get("/",(req,res)=>{
     res.status(200).json({
