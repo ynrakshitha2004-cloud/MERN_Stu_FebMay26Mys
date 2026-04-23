ffd4d058d36e5ab668ffb4eae27f23326a3307c3
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-23 10:55:51 +0530

    Added shows routes

--- projects/Guided project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ projects/Guided project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const authRoutes = require("./src/routes/auth.routes");
-const movieRoutes = require("./src/routes/movie.routes")
-const bookingRoutes = require("./src/routes/booking.routes")
+const movieRoutes = require("./src/routes/movie.routes");
+const bookingRoutes = require("./src/routes/booking.routes");
+const showRoutes = require("./src/routes/show.routes");
+
 const app = express();
 
 app.use(express.json());
 app.use("/api/auth",authRoutes);
 app.use("/api/movies",movieRoutes);
 app.use("/api/bookings",bookingRoutes);
+app.use("/api/shows",showRoutes);
+
 //Base URL
 app.get("/",(req,res)=>{
     res.status(200).json({
@@ -15,6 +19,4 @@ app.get("/",(req,res)=>{
         message: "Movie booking API is running...",
     });
 });
-
-
 module.exports = app;
