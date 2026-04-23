9a5a8e4df204da838f48c5876f9da2f2d2d40dbd
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-23 15:34:33 +0530

    Added routes for create, get & cancel booking

--- projects/Guided project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/booking.routes.js
+++ projects/Guided project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/booking.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {protect} =require("../middleware/auth.middleware");
+const bookingController = require("../controllers/booking.controller");
+const { protect } = require("../middleware/auth.middleware");
 //user booking route
-router.post("/",protect,(req,res)=>{
-    res.send("Created booking");
-});
+router.post("/", protect, bookingController.createBooking);
+router.get("/my", protect, bookingController.getMyBooking);
+router.post("/:id", protect, bookingController.cancelBooking);
+module.exports = router;
