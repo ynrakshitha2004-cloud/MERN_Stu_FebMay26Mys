cb52db7e1aeb9215d6211967903e14d2a60fd9e1
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-26 20:20:03 +0530

    Portfolio features V3.0

---
+++ Assignment/portfolio-project V3.0/js/data/education.js
@@ -0,0 +1,23 @@
+const educationData = [
+  {
+    institution: "Cauvery institute of technology,Mandya",
+    degree: "Bachelor of Engineering in Computer science",
+    duration: "Dec 2022 - May 2026",
+    location: "Mandya, Karnataka",
+    score: "CGPA: 8.7"
+  },
+  {
+    institution: "Viveka PU college,sargur,HD kote",
+    degree: "Pre-University (PUC)",
+    duration: "2020 - 2022",
+    location: "Mysore, Karnataka",
+    score: "Percentage: 85%"
+  },
+  {
+    institution: "Adarsha vidyalaya(RMSA),kollegal",
+    degree: "SSLC (10th)",
+    duration: "2020",
+    location: "Chamarajnagar, Karnataka",
+    score: "Percentage: 93.12%"
+  }
+];
