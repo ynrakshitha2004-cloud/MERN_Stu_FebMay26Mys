26751bb1e90eac59c51b873296138e8aa3f2d2e0
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-29 10:30:26 +0530

    Prop drilling

---
+++ W9/D2/my-app/prop-example-app/src/components/p4.jsx
@@ -0,0 +1,36 @@
+import { React } from "react";
+// Prop drilling
+function DrillLayout({ user }){
+    return(
+        <div className="layout">
+            <DrillSidebar user={user}/>
+        </div>
+    )
+}
+function DrillSidebar({ user }){
+    return(
+        <aside className="sidebar">
+            <DrillUserPanel user={user} />
+        </aside>
+    )
+}
+function DrillUserPanel({user}){
+    return(
+        <section className="user-panel">
+            <ProfileAvatar user={user} />
+        </section>
+    )
+}
+function ProfileAvatar({ user }){
+    return <img src={`/${user.name}.png`} alt={user.name} />
+}
+
+export function PropDrillingDemo(){
+    const user = {name: 'Rakshitha'};
+    return(
+        <div>
+            <h3>Problem: prop drilling</h3>
+            <DrillLayout user={user} />
+        </div>
+    )
+}
