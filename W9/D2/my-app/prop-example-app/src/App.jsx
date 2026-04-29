5b7c753530c20602e91b23956adfc63aa946cc25
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-29 11:45:46 +0530

    State LIfting

--- W9/D2/my-app/prop-example-app/src/App.jsx
+++ W9/D2/my-app/prop-example-app/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
-import { PropBasics } from "./components/p1"
+import { PropBasics } from "./components/p1";
+import { PropDestructuring } from "./components/p2";
+import { PropsChildren } from "./components/p3";
+import { PropDrillingDemo } from "./components/p4";
+import { SharedStateParent } from "./components/p5";
 function App() {
-
   return (
     <>
-      <PropBasics />
+       {/* <PropBasics /> */}
+      {/* <PropDestructuring /> */}
+      {/* <PropsChildren /> */}
+      {/* <PropDrillingDemo /> */}
+      <SharedStateParent/>
     </>
   )
 }
-
 export default App
