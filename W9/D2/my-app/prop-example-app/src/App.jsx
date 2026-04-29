e3400f292e363341da0c0ecabfd97b0403621a46
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-29 13:50:32 +0530

    Props immutable

--- W9/D2/my-app/prop-example-app/src/App.jsx
+++ W9/D2/my-app/prop-example-app/src/App.jsx
@@ -6,6 +6,7 @@ import { PropDrillingDemo } from "./components/p4";
 import { SharedStateParent } from "./components/p5";
 import { CompositionDemo } from "./components/p6";
 import { ObjectsAndArrayProps } from './components/p7';
+import { PropsImmutability } from './components/p8';
 function App() {
   return (
     <>
@@ -15,7 +16,8 @@ function App() {
       {/* <PropDrillingDemo /> */}
       {/* <SharedStateParent/> */}
       {/* <CompositionDemo/> */}
-      <ObjectsAndArrayProps /> 
+      {/* <ObjectsAndArrayProps />  */}
+      <PropsImmutability />
       </>
   )
 }
