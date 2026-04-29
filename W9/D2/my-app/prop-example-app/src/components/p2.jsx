9720d11467c67766bfd0fb7079c535388c671ee2
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-04-29 10:29:27 +0530

    Props destructuring

--- W9/D2/my-app/prop-example-app/src/components/p2.jsx
+++ W9/D2/my-app/prop-example-app/src/components/p2.jsx
@@ -1,18 +1,20 @@
+// Props destructuring
 import { React } from "react";
-function UserProfile({ username, skill }) {
-    return (
+//Child component
+function UserProfile({username,skill}){
+    return(
         <div>
             <p>User: {username}</p>
-            <p>role: { skill}</p>
+            <p>Skill: {skill}</p>
         </div>
     )
 }
-//Parent component
-export function  PropDestructing(){
+// Parent component
+export function PropDestructuring(){
     return(
         <>
-<h2>Prop Destructing</h2>
-<UserProfile username="Rakshitha" skill="React" />
-</>
-    )
+        <h2>Prop Destructuring</h2>
+        <UserProfile username="Rakshitha" skill="React" />
+        </>
+    );
 }
