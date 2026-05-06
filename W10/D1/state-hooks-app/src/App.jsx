0586efdb9d4afd46a31deaa153386562b8ec5c8b
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-05-06 09:35:00 +0530

    added W10/D1 state & hooks app

---
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { UseStateBasics } from "./components/P1";
+import { UpdatingObjectsArraysState } from "./components/P2";
+import { EventHandlingState } from "./components/P3";
+import { DerivedStateCondRender } from "./components/P4";
+function App() {
+  return (
+    <>
+      {/* <UseStateBasics /> */}
+      {/* <UpdatingObjectsArraysState /> */}
+      {/* <EventHandlingState /> */}
+      <DerivedStateCondRender />
+    </>
+  )
+}
+export default App
