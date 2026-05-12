d3af7e7629fb58889147245dceb251920d0560a9
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-05-12 10:01:16 +0530

    Protected Route file added

---
+++ W10/D4/my-app/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,8 @@
+import { Navigate } from "react-router-dom";
+
+export function ProtectedRoute({isAuthenticated,children}) {
+    if (!isAuthenticated) {
+        return <Navigate to="/" />;
+    }
+    return children;
+}
