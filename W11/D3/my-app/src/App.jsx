14e5eb23d3946011180a5ac074bb7b7213f3a97d
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-05-13 15:12:12 +0530

    Managing API state with useEffect

---
+++ W11/D3/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {ManagingApiState} from "./components/p1";
+import { UseEffectLifecycle } from './components/p2';
+function App() {
+ 
+  return (
+    <>
+     {/* < ManagingApiState/> */}
+     <UseEffectLifecycle/>
+    </>
+  )
+}
+
+export default App
