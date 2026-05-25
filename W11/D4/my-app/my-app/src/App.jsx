6e2a16f7caf6c55e2a2105b92a9e05f16ab3087b
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-05-25 16:26:34 +0530

    Async form submission

---
+++ W11/D4/my-app/my-app/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { ControlledForm } from "./components/p1";
+import { ValidationForm } from "./components/p2";
+import { AsyncForm } from "./components/p3";
+
+function App() {
+  
+  return (
+    <>
+      {/* <ControlledForm/> */}
+      {/* <ValidationForm /> */}
+      <AsyncForm/>
+
+    </>
+  )
+}
+
+export default App
